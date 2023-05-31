5258492f7d58f085bc275519835c6f363fce7582
Kuroky <optimusss247@gmail.com>
2023-05-31 15:39:18 +0700

    me may tien di can gio bao vcl

--- app/src/main/java/com/example/hello/MainActivity.java
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String ditmemaytien="tu tien sang lui";
-        String quevl="hello qq";
+
     }
 }
